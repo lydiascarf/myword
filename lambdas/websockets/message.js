59700118dc7fb4b7486bcc0bf588033951436e2a
Jacob Scarf Merrell <jacobscarfmerrell@gmail.com>
2020-05-15 15:44:17 -0400

    ADD: eslint

--- lambdas/websockets/message.js
+++ lambdas/websockets/message.js
@@ -5,34 +5,34 @@ const WebSocket = require('../common/WebSocketMessage');
 const tableName = process.env.tableName;
 
 exports.handler = async event => {
-    console.log('event', event);
+  console.log('event', event);
 
-    const { connectionId } = event.requestContext;
+  const { connectionId } = event.requestContext;
 
-    const body = JSON.parse(event.body);
+  const body = JSON.parse(event.body);
 
-    try {
-        const record = await Dynamo.get(connectionId, tableName);
-        const { domainName, messages, stage } = record;
+  try {
+    const record = await Dynamo.get(connectionId, tableName);
+    const { domainName, messages, stage } = record;
 
-        messages.push(body.message);
+    messages.push(body.message);
 
-        const data = {
-            ...record,
-            messages
-        };
+    const data = {
+      ...record,
+      messages
+    };
 
-        await Dynamo.write(data, tableName);
+    await Dynamo.write(data, tableName);
 
-        await WebSocket.send({
-            connectionId,
-            domainName,
-            message: "This is a reply to your message",
-            stage,
-        })
+    await WebSocket.send({
+      connectionId,
+      domainName,
+      message: "This is a reply to your message",
+      stage,
+    })
 
-        return Responses._200({ message: 'got a message' });
-    } catch (error) {
-        return Responses._400({ message: 'message could not be received' })
-    }
+    return Responses._200({ message: 'got a message' });
+  } catch (error) {
+    return Responses._400({ message: 'message could not be received' })
+  }
 }
