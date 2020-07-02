9a8777f1cb8cc27d156ae277e4100a1ae7cff9c4
Jacob Scarf Merrell <jacobscarfmerrell@gmail.com>
2020-07-02 12:41:53 -0400

    DELETE: lambda code

--- ARCHIVED_lambdas/websockets/default.js
+++
@@ -1,7 +0,0 @@
-const Responses = require('../common/API_Responses');
-
-exports.handler = async event => {
-    console.log('event', event);
-
-    return Responses._200({ message: 'default' });
-};
