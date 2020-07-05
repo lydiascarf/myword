2f8bb1ab9ed2e5ba0ba4972f3d0c2dc0bb174ee1
Nancy Alter <nancy.alter@gmail.com>
2020-07-05 10:04:54 -0400

    update data file names
    replace 'my' and 'your' with 'blue' and 'beige'

--- src/components/MyBoard/index.js
+++ src/components/MyBoard/index.js
@@ -5,7 +5,7 @@ import data from '../../data/2_board.json';
 function MyBoard() {
     
     return (<div>
-        <TurnTable isMine turnRecords={data.board.myturns}/>
+        <TurnTable isMine turnRecords={data.board.blueturns}/>
     </div>
     );
 }
